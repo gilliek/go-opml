9c26f17c4854b9261b818e2c9fa5985b1e2792c6
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-06-06 20:36:08 +0200

    add comments

--- opml/opml.go
+++ opml/opml.go
@@ -2,6 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+/*
+Package opml provides all the required structures and functions for parsing
+OPML files, as defined by the specification of the OPML format:
+
+	[OPML 1.0] http://dev.opml.org/spec1.html
+	[OPML 2.0] http://dev.opml.org/spec2.html
+
+It is able to parse both, OPML 1.0 and OPML 2.0, files.
+*/
 package opml
 
 import (
@@ -10,12 +19,15 @@ import (
 	"net/http"
 )
 
+// OPMLNode is the root node of an OPML document. It only has a single required
+// attribute: the version.
 type OPMLNode struct {
 	Version string   `xml:"version,attr"`
 	Head    HeadNode `xml:"head"`
 	Body    BodyNode `xml:"body"`
 }
 
+// HeadNode holds some meta information about the document.
 type HeadNode struct {
 	Title           string `xml:"title"`
 	DateCreated     string `xml:"dateCreated,attr"`
@@ -32,10 +44,12 @@ type HeadNode struct {
 	WindowRight     string `xml:"windowRight,attr"`
 }
 
+// BodyNode is the parent structure of all outlines.
 type BodyNode struct {
 	Outlines []OutlineNode `xml:"outline"`
 }
 
+// OutlineNode holds all information about an outline.
 type OutlineNode struct {
 	Outlines     []OutlineNode `xml:"outline"`
 	Text         string        `xml:"text,attr"`
@@ -52,10 +66,12 @@ type OutlineNode struct {
 	Description  string        `xml:"description,attr"`
 }
 
+// OPML represents an OPML document.
 type OPML struct {
 	Root *OPMLNode
 }
 
+// NewOPML creates a new OPML structure from a slice of bytes.
 func NewOPML(b []byte) (*OPML, error) {
 	var root OPMLNode
 	err := xml.Unmarshal(b, &root)
@@ -66,6 +82,7 @@ func NewOPML(b []byte) (*OPML, error) {
 	return &OPML{Root: &root}, nil
 }
 
+// NewOPMLFromURL creates a new OPML structure from an URL.
 func NewOPMLFromURL(url string) (*OPML, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -81,6 +98,7 @@ func NewOPMLFromURL(url string) (*OPML, error) {
 	return NewOPML(b)
 }
 
+// NewOPMLFromFile creates a new OPML structure from a file.
 func NewOPMLFromFile(filePath string) (*OPML, error) {
 	b, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -90,6 +108,7 @@ func NewOPMLFromFile(filePath string) (*OPML, error) {
 	return NewOPML(b)
 }
 
+// Outlines returns a slice of the outlines.
 func (doc *OPML) Outlines() []OutlineNode {
 	return doc.Root.Body.Outlines
 }
